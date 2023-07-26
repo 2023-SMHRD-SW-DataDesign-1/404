bbcc82885fdaa71603410f846319141a77acfee2
smhrd <smhrd@211.227.224.198>
2023-07-26 15:34:40 +0900

    07/26 15:34 김지홍_일단 커밋

--- 404project/src/main/java/com/smhrd/model/FeedCommentDTO1.java
+++ 404project/src/main/java/com/smhrd/model/FeedCommentDTO1.java
@@ -3,11 +3,13 @@ package com.smhrd.model;
 import java.sql.Timestamp;
 
 public class FeedCommentDTO1 {
-    private int commentNo;
+
+	private int commentNo;
     private int feedNo;
     private String profileImage;
     private String nickname;
     private String text;
-    private int likes;
     private Timestamp time;
+    
+    
 }
