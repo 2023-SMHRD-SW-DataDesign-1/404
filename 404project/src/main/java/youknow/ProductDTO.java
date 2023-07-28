c5c60429de3422c8ac272f2548f17ebf2d96d3d1
Smhrdd <Smhrdd@211.63.240.66>
2023-07-28 16:01:08 +0900

    0728 16:00 이윤호 _ MemberDTO , ProductDTO, ProductDAO, ProductMapper.xml

--- 404project/src/main/java/youknow/ProductDTO.java
+++ 404project/src/main/java/youknow/ProductDTO.java
@@ -10,12 +10,32 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductDTO {
+	int product_no;
+	String product_name;
+	String product_image1;
+	String product_image2;
+	String product_image3;
+	String product_image4;
+	String product_image5;
+	String product_image6;
+	String product_image7;
+	String product_image8;
+	String product_image9;
+	String product_image10;
+	int product_price;
+	int product_stock1;
+	String product_info;
+	String main_category;
+	String sub_category;
+	String ai_category;
+	String product_option3;
+	
+//	int item_number;
+//	String item_name;
+//	String item_image;
+//	int item_price;
+//	int item_quantity;
+//	String item_info;
+//	String item_category;
 
-	int item_number;
-	String item_name;
-	String item_image;
-	int item_price;
-	int item_quantity;
-	String item_info;
-	String item_category;
 }// CLASS
