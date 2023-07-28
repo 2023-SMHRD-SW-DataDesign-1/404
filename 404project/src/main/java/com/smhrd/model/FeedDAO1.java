ac3fb35d5128c76b7705b0587b43159c48b425e3
smhrd <smhrd@119.200.31.199>
2023-07-28 16:24:29 +0900

    0728 4:24 김드보라_main 수정

--- 404project/src/main/java/com/smhrd/model/FeedDAO1.java
+++ 404project/src/main/java/com/smhrd/model/FeedDAO1.java
@@ -96,7 +96,7 @@ public class FeedDAO1 {
 		SqlSession session = sqlSessionFactory.openSession(true);
 
 		// 2. sql문장 실행하기
-		ArrayList lfeed_list = (ArrayList) session.selectList("com.smhrd.mapper.FeedMapper.showAllLikesFeeds");
+		ArrayList lfeed_list = (ArrayList) session.selectList("showAllLikesFeeds");
 
 		// 3. 연결 종료하기
 		session.close();
